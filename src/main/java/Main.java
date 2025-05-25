dd453d2442d46c0e1653472120bb366d9f3698e1
Wallace Oliveira <wallaceoliv05@gmail.com>
2025-05-25 11:34:21 -0300

    - Debug Mode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,21 @@ import Uteis.Uteis;
 import java.util.List;
 
 public class Main {
+
+    public static boolean _IS_DEBUG = false;
+
     public static void main(String[] args) {
 
-//        if (args.length != 4) {
-//            System.out.println("Tente: java Main <id_node> <ip_node> <port_node> <is_service_printer>");
-//            throw  new RuntimeException();
-//        }
-//
-//        NodeInfo currentNodeInfo = new NodeInfo(args[0], args[1], Integer.parseInt(args[2]));
-//        boolean isServer = Boolean.getBoolean(args[3]);
+        if (args.length != 4) {
+            System.out.println("Tente: java Main <id_node> <ip_node> <port_node> <is_service_printer>");
+            _IS_DEBUG = true;
+        }
 
-        Boolean isServer = true;
-        NodeInfo currentNodeInfo = new NodeInfo("Service", "192.168.0.2", 5000);
+        NodeInfo currentNodeInfo = (_IS_DEBUG) ?
+                new NodeInfo("Service", "192.168.0.2", 5000)
+                : new NodeInfo(args[0], args[1], Integer.parseInt(args[2]));
 
+        boolean isServer = _IS_DEBUG || Boolean.getBoolean(args[3]);            
 
         List<NodeInfo> nodesList = Uteis.readNodes("configNodes.txt");
         NodeInfo service = Uteis.readService("serviceNode.txt");
